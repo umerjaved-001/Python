2bd7065b0169d00ab0f8b919c35b449c151334fe
Umer Javed <b25f1433se072@paf-iast.edu.pk>
2026-07-15 08:00:53 +0500

    Create Task_2.py

---
+++ Internship_Work/Assignment_2/Tuesday/Task_2.py
@@ -0,0 +1,11 @@
+def largest(a,b)
+   if a > b :
+   return a
+   else :
+   return b
+
+a = input("Enter value of a:")
+b = input("Enter value of b:")
+
+Result = largest(a,b)
+print("Largest number:",Result)
