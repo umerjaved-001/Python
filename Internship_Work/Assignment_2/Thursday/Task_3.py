d42364b21c8595df1119c997f6398803fa9f0da9
umerjaved-001 <b25f1433se072@paf-iast.edu.pk>
2026-07-15 11:57:32 +0500

    Update

--- Internship_Work/Assignment_2/Thursday/Task_3.py
+++ Internship_Work/Assignment_2/Thursday/Task_3.py
@@ -6,5 +6,5 @@ sub_2 = float(input("Enter marks of Subject 2 :"))
 sub_3 = float(input("Enter marks of Subject 3 :"))
 
 Result = calculate_total(sub_1,sub_2,sub_3)
-print("Total Marks:",Result)
+print("Total Marks: ",Result)
 
